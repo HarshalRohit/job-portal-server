6ce5e117ee0b6e7a6818ff0f02e4425072180678
HarshalRohit <rohitharshal95@gmail.com>
2020-07-14 18:48:42 +0530

    fixed .env not found

--- config/index.js
+++ config/index.js
@@ -5,10 +5,10 @@ const dotenv = require('dotenv');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// if (envFound.error) {
+//   // This error should crash whole process
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 exports.port = process.env.PORT;
 exports.api= {
